2dac847c037471cec9dabedcfde2cafbdc982468
codeYann <ccyan05@alu.ufc.br>
2023-01-29 11:03:56 -0300

    Importing guessing_game module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod guessing_game;
+
+fn greetings(a: String) {
+    println!("this is my string: {}", a);
+}
+
 fn main() {
     println!("Hello, world!");
+    greetings("My name is Yan!".to_string());
+    guessing_game::make_guess();
 }
