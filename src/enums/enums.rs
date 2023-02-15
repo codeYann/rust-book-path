5e6bac97a3afbd28d7b3ddec715261cf37693741
codeYann <ccyan05@alu.ufc.br>
2023-02-15 09:32:49 -0300

    Learning about enums

--- src/enums/enums.rs
+++ src/enums/enums.rs
@@ -1,26 +1,26 @@
 #[derive(Debug)]
-pub enum IpAddrKind {
-    V4(i32),
-    V6(i32),
+pub enum _IpAddrKind {
+    _V4(i32),
+    _V6(i32),
 }
 
 pub fn __exec_ip_addr_kind() {
-    let four = IpAddrKind::V4(127_000_1);
-    let six = IpAddrKind::V6(012_125_221);
+    let four = _IpAddrKind::_V4(127_000_1);
+    let six = _IpAddrKind::_V6(012_125_221);
 
     println!("{:?}, {:?}", four, six);
 }
 
 #[derive(Debug)]
-pub enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+pub enum _Message {
+    _Quit,
+    _Move { x: i32, y: i32 },
+    _Write(String),
+    _ChangeColor(i32, i32, i32),
 }
 
-impl Message {
-    pub fn call(&self) {
+impl _Message {
+    pub fn _call(&self) {
         println!("ola mundo");
     }
 }
