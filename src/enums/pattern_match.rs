04ea17e1612c5e554383dcc6d5c3d46bfe9c3a2f
codeYann <ccyan05@alu.ufc.br>
2023-02-15 10:12:55 -0300

    Learning about pattern match in rust

--- src/enums/pattern_match.rs
+++ src/enums/pattern_match.rs
@@ -14,3 +14,20 @@ pub fn _value_in_cents(coin: &_Coins) -> u8 {
         &_Coins::Quarter => 25,
     };
 }
+
+pub fn _matching_things() -> String {
+    let name = "";
+    return match name {
+        "name" => String::from("Ai calica"),
+        &_ => String::from("Dude"),
+    };
+}
+
+pub fn _if_let_short_hand() {
+    let value = _Coins::Penny;
+    if let _Coins::Penny = value {
+        println!("I told long ago");
+    } else {
+        println!("You're wrong!");
+    }
+}
