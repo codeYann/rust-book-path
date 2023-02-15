d074cb2985136dc94a8dcfeb1980ba500eafc97c
codeYann <ccyan05@alu.ufc.br>
2023-02-15 09:32:19 -0300

    Main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod enums;
+
 fn main() {
     println!("Hello, World!");
+
+    let enumeration = enums::pattern_match::_Coins::Dime;
+    let result = enums::pattern_match::_value_in_cents(&enumeration);
+    println!("{:?}, {}", enumeration, result);
+
+    let enumeration = enums::pattern_match::_Coins::Nickel;
+    let result = enums::pattern_match::_value_in_cents(&enumeration);
+    println!("{:?}, {}", enumeration, result);
+
+    let enumeration = enums::pattern_match::_Coins::Penny;
+    let result = enums::pattern_match::_value_in_cents(&enumeration);
+    println!("{:?}, {}", enumeration, result);
+
+    let enumeration = enums::pattern_match::_Coins::Quarter;
+    let result = enums::pattern_match::_value_in_cents(&enumeration);
+    println!("{:?}, {}", enumeration, result);
+
 }
