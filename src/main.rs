3741907fcd61df4b621daf4db56c47e42c7437f8
codeYann <ccyan05@alu.ufc.br>
2023-02-21 15:10:28 -0300

    Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,9 @@
-// I can define an inline mod and use it only in main.rs file
-// When a module is defined the compiler will look for module's code in this order:
-// inline
-// src/garden.rs
-// src/garden/mod.rs -> That's the way i've been using
-
-mod garden {
-
-    pub fn green() -> i32 {
-        5
-    }
-    // It's possible to create a sub module inside a module
-    pub mod veg {
-        #[derive(Debug)]
-        pub struct Field {}
-        pub fn creating_it() -> Field {
-            return Field {};
-        }
-
-        pub fn shows_it() {
-            let var = super::green();
-            println!("{}", var);
-        }
-    }
-
-    pub fn creating_it() -> String {
-        return "ola mundo".to_string();
-    }
-}
-
-use garden::veg;
+mod collect;
 
 fn main() {
-    let it = veg::Field {};
-    println!("Hello, World!");
-    let value = garden::creating_it();
-    println!("{}", &value);
-    let value = garden::veg::creating_it();
-    println!("{:?}", &value);
-    println!("{:?}", it);
-
-    veg::shows_it();
+    collect::vects::dealing_with_vectors();
+    collect::vects::reading_values_from_vecs();
+    collect::vects::borrowing_in_vec();
+    collect::vects::iterating_over_a_vec();
+    collect::vects::vecs_and_enums();
 }
