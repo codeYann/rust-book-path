88a0e29e6d1cf9abc01239f2b2f165bc90a91e16
codeYann <ccyan05@alu.ufc.br>
2023-03-02 13:19:15 -0300

    Using ? operador, Option and Result enums

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-mod collect;
+// mod errors_h;
+// use errors_h::recoverable_erros;
 
-use collect::hashmaps;
+fn to_int(s: &str) -> Option<i32> {
+    return s.parse().ok();
+}
+
+fn sum_str_vec(strs: &Vec<String>) -> String {
+    let mut acc: i32 = 0;
+
+    for s in strs {
+        acc += match to_int(&s) {
+            Some(number) => number,
+            None => 0,
+        }
+    }
+
+    return acc.to_string();
+}
 
 fn main() {
-    hashmaps::__updating_hash_map();
-    hashmaps::__entry_method();
-    hashmaps::__updating_based_old_values();
+    let buffer = vec![
+        "Once upon a time".to_string(),
+        "1".to_string(),
+        "2".to_string(),
+    ];
+    let value = sum_str_vec(&buffer);
+    println!("{}", value);
 }
