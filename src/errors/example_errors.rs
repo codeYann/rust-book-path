0b603d585253416c116beb116d16d4ec6ad8db78
codeYann <ccyan05@alu.ufc.br>
2023-03-03 11:30:06 -0300

    Using option and result enums

--- src/errors/example_errors.rs
+++ src/errors/example_errors.rs
@@ -1,16 +1,29 @@
-fn _to_int(s: &str) -> Option<i32> {
-    return s.parse().ok();
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+pub struct SummationError;
+
+fn to_int(s: &str) -> Result<i32, ParseIntError> {
+    return s.parse();
 }
 
-fn _sum_str_vec(strs: &Vec<String>) -> String {
+fn sum_str_vec(strs: &Vec<String>) -> Result<String, SummationError> {
     let mut acc: i32 = 0;
 
-    for s in strs {
-        acc += match _to_int(&s) {
-            Some(number) => number,
-            None => 0,
-        }
+    for str in strs {
+        acc += to_int(&(*str)).map_err(|_| SummationError)?;
     }
 
-    return acc.to_string();
+    return Ok(acc.to_string());
+}
+
+pub fn __exec_it() {
+    let buffer = vec!["1".to_string(), "2".to_string(), "3".to_string()];
+    let value = sum_str_vec(&buffer);
+    println!("{:?}, {:?}", value, buffer);
+
+    let buffer = vec!["abc".to_string()];
+
+    let value = sum_str_vec(&buffer);
+    println!("{:?}, {:?}", value, buffer);
 }
