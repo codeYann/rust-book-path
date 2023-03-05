3214e956091ea0a555c7eb3646ed4d6705322c00
codeYann <ccyan05@alu.ufc.br>
2023-03-05 10:32:00 -0300

    Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod generics;
-use generics::gen;
+use generics::traits::{Tweet, _exec_it, self};
 
 fn main() {
-    gen::_exec_largest();
-    gen::_exec_point();
+    let my_tweet = Tweet {
+        _username: "codeYan".to_string(),
+        _content: "This is my first tweet :))".to_string(),
+        _reply: false,
+        _retweet: false,
+    };
+
+    let value = traits::_test_it(&my_tweet);
+
+    println!("{}", value);
+
+    _exec_it();
 }
