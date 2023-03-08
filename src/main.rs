06145c0232d791a51cb921f168175f0c0137f36f
codeYann <ccyan05@alu.ufc.br>
2023-03-07 22:05:05 -0300

    Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
 mod generics;
-use generics::traits_examples::{MyEnum, MyTrait};
 
-// Writting a function to consume this particular .sum() function
-// It's important to notice that consume_this_trait receive a &impl MyTrait as a param
-// in other words any type that implements MyTrait is able to use this function
-// also consume_this_trait returns a Result<i32, ()> that means that we can use question mark
-// operator
-fn consume_this_trait(it: &impl MyTrait) -> Result<i32, ()> {
-    let v = it.sum()?;
-    return Ok(v);
-}
+// use generics::lifetimes_examples::Node;
 
-fn main() -> Result<(), ()> {
-    let v = MyEnum::Counter(15);
-    let it = consume_this_trait(&v)?;
+fn main() {
+    
+    let a = "Hello, world".to_string();
+    let b = a;
 
-    println!("{it}");
-    return Ok(());
+    println!("{}", a);
 }
